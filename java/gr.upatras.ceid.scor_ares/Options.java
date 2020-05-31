ac8797c6db79c39d9187967a29f354e3881ece37
TsChrys <31798837+TsChrys@users.noreply.github.com>
2020-05-31 15:01:02 +0300

    Add files via upload

--- java/gr.upatras.ceid.scor_ares/Options.java
+++ java/gr.upatras.ceid.scor_ares/Options.java
@@ -4,42 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-public class Options extends Fragment //implements View.OnClickListener
-{
+public class Options extends Fragment {
+
+    Button accountSettingsBtn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        /*
-        Button accountSettings = (Button) View.findViewById(R.id.accountSettings);
-        accountSettings.setOnClickListener(this);
+        View view = inflater.inflate(R.layout.options_layout, container, false);
 
-         */
-        return inflater.inflate(R.layout.options_layout, container, false);
+        accountSettingsBtn = view.findViewById(R.id.accountSettings);
+        accountSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AccountSettings accountSettings = new AccountSettings();
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, accountSettings);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
-
-    /*
-    @Override
-    public void onClick(View view) {
-        Fragment fragment = null;
-        if (view.getId() == R.id.accountSettings){
-            fragment = new AccountSettings();
-            replaceFragment(fragment);
-        }
-    }
-
-    public void replaceFragment(Fragment fr) {
-        FragmentTransaction transaction = FragmentActivity.getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container_fragment,fr);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
-     */
 }
